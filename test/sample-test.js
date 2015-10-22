fe5a8cf97b8541cd2b1ad4dcf0f494e3c2906ca1
Renjith G R <grenjithr@gmail.com>
2015-10-22 15:43:08 +0530

    Setup for tests

---
+++ test/sample-test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+
+describe('Sample Test', function () {
+    it('1 + 1 should be 2', function (done) {
+        assert.equal(2, 1+1);
+        done();
+    });
+});
