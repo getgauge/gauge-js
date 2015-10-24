d4a04a0a5e702dd3a3d8ffa657980dc95e93c29d
Renjith G R <grenjithr@gmail.com>
2015-10-25 03:48:38 +0530

    Adding doc for generalise() in step-parser.js

--- step-parser.js
+++ step-parser.js
@@ -1,5 +1,13 @@
 exports = module.exports;
 
+/**
+ * Returns the generalised form of a step description.
+ * Example:
+ * Generalised form of the step "Say <greeting> to <user>" is "Say {} to {}".
+ *
+ * @param  {[String]} stepName Description of the step.
+ * @return {[String]}          Generalised form of the step.
+ */
 exports.generalise = function(stepName) {
   return stepName.replace(/(<.*?>)/g, '{}');
 };
