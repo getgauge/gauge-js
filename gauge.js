d766296e5127ce99fc6cbe3a16fce405ee777c9e
Renjith G R <grenjithr@gmail.com>
2015-10-31 21:53:29 +0530

    Step implementations are now read from the project directory

--- gauge.js
+++ gauge.js
@@ -1,8 +1,13 @@
+var connection = require('./connection');
+var impl_loader = require('./impl-loader');
+require('./gauge-global');
+
+var GAUGE_INTERNAL_PORT = process.env.GAUGE_INTERNAL_PORT;
+var GAUGE_PROJECT_ROOT = process.env.GAUGE_PROJECT_ROOT;
+
 function run() {
-  var connection = require('./connection');
-  require('./gauge-global');
-  require('./impl-loader');
-  new connection('localhost', process.env.GAUGE_INTERNAL_PORT).run();
+  impl_loader.load(GAUGE_PROJECT_ROOT);
+  new connection('localhost', GAUGE_INTERNAL_PORT).run();
 }
 
 module.exports= {
--- impl-loader.js
+++ impl-loader.js
@@ -1,6 +1,12 @@
 require('./gauge-global');
 var fileUtil = require('./file-util');
 
-fileUtil.getListOfFilesFromPath(__dirname + '/' + 'step_impl').forEach(function(filePath) {
+function loadImpl(projectRoot) {
+  fileUtil.getListOfFilesFromPath(projectRoot + '/' + 'step_implementations').forEach(function(filePath) {
     require(filePath);
-});
+  });
+}
+
+module.exports= {
+  load: loadImpl
+}
