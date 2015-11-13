6520de6cb5dff69a9d6178069568af6f99410de0
Renjith G R <grenjithr@gmail.com>
2015-11-13 19:16:58 +0530

    Step executor, added more stuff. WIP

--- test/step-executor.js
+++ test/step-executor.js
@@ -3,13 +3,39 @@ var sinon  = require('sinon');
 var StepExecutor = require('../lib/step-executor');
 
 describe('Step Execution', function() {
-  var fn = sinon.spy();
 
   it('should call the function with specified parameters', function(done) {
+    var fn = sinon.spy();
     var params = [1,2,3];
     StepExecutor.execute(fn, params);
     assert(fn.calledOnce);
     assert.deepEqual(params, fn.getCall(0).args);
     done();
   });
+
+  it('should send done function as the last parameter', function(done) {
+    var fn = sinon.spy();
+    var params = [1,2];
+    StepExecutor.execute(fn, params);
+    var actualArgsLength = fn.getCall(0).args.length;
+    assert.equal(3, actualArgsLength);
+    done();
+  });
+
+  it('should return error response if function fails', function(done) {
+    var error = new Error('Whoops!');
+    var fn = function() {
+      throw error;
+    };
+
+    var result = StepExecutor.execute(fn, []);
+    assert.deepEqual({result: "failure", exception: error}, result);
+    done();
+  });
+
+  it('should return success response if function succeeds', function() {
+    var result = StepExecutor.execute(function() {}, []);
+    assert.deepEqual({result: "success"}, result);
+  });
+
 });
