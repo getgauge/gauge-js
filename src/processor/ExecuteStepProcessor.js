95c243976c60f16d850152ab2be6d7ddb5b2aec2
Renjith G R <grenjithr@gmail.com>
2015-11-24 09:57:28 +0530

    Cleaning up ExcecuteStepProcessor.js

--- src/processor/ExecuteStepProcessor.js
+++ src/processor/ExecuteStepProcessor.js
@@ -30,27 +30,6 @@ var executeStep = function(request) {
       deferred.reject(errorResponse);
     }
   );
-  // StepExecutor.execute(stepRegistry.get(parsedStepText), parameters, function(result) {
-  //   if(result.result === "success") {
-  //     var response = executionResponse(false, 0, request.messageId);
-  //     deferred.resolve(response);
-  //   } else if (result.result === "false") {
-  //     var errorResponse = executionResponse(true, 0, request.messageId);
-  //     deferred.resolve(errorResponse);
-  //   }
-  // });
-
-  // try {
-  //   var parameters = request.executeStepRequest.parameters.map(function(item) {
-  //     return item['value'] ? item['value'] : item['table'];
-  //   });
-  //   stepRegistry.get(parsedStepText).apply(null, parameters);
-    // var response = executionResponse(false, 0, request.messageId);
-    // deferred.resolve(response);
-  // } catch (error) {
-  //   var errorResponse = executionResponse(true, 0, request.messageId);
-  //   deferred.resolve(errorResponse);
-  // }
 
   return deferred.promise;
 };
