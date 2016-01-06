8df0cdc841ec620a1ece89d857a7c01f9016124f
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-06 14:23:47 +0530

    Check lint and style for index.js
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 #! /usr/bin/env node
 
-var fs = require('fs'),
-    path = require('path');
+var fs = require("fs"),
+    path = require("path");
 
-var skeldir = path.join(__dirname, 'skel'),
-    srcdir = path.join(process.env.GAUGE_PROJECT_ROOT, 'src'),
-    testCode = 'step_implementation.js';
+var skeldir = path.join(__dirname, "skel"),
+    srcdir = path.join(process.env.GAUGE_PROJECT_ROOT, "src"),
+    testCode = "step_implementation.js";
 
-if(process.argv[2] === '--init') {
+if(process.argv[2] === "--init") {
 
-    console.log("Initialising Gauge JavaScript project");
-    fs.mkdir(srcdir, 484, function(err) {
-        if (err && err.code !== 'EEXIST') {
-            console.error(err);
-        } else {
-            fs.createReadStream(path.join(skeldir, testCode))
-                .pipe(fs.createWriteStream(path.join(srcdir, testCode)));
-        }
-    });
+  console.log("Initialising Gauge JavaScript project");
+  fs.mkdir(srcdir, 484, function(err) {
+    if (err && err.code !== "EEXIST") {
+      console.error(err);
+    } else {
+      fs.createReadStream(path.join(skeldir, testCode))
+        .pipe(fs.createWriteStream(path.join(srcdir, testCode)));
+    }
+  });
 }
 
-else if(process.argv[2] === '--start') {
-    require('./src/gauge').run();
+else if(process.argv[2] === "--start") {
+  require("./src/gauge").run();
 }
