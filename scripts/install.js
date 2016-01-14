4537ed8da5681bad556b16a3e73ac3e082996515
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-14 14:47:46 +0530

    Add package script and refactor install
    To install plugin, run:
    
        $ npm run pluginInstall
    
    To create plugin package, run:
    
        $ npm run package
    
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,7 @@
 var fs = require("fs-extra"),
     path = require("path"),
+    archiver = require("archiver"),
+    child_process = require("child_process"),
     CWD = process.cwd();
 
 var localPath = function (relativePath) {
@@ -12,33 +14,101 @@ var homeDir = process.env[(process.platform == "win32") ? "USERPROFILE" : "HOME"
 
 var pluginInstallDir = path.resolve(homeDir, ".gauge", "plugins", plugin.id, plugin.version);
 
-var cleanInstallDir = function () {
+var cleanDir = function (dirPath) {
   try {
-    fs.removeSync(pluginInstallDir);
+    fs.removeSync(dirPath);
   } catch (err) {
-    console.error("Error removing plugin installation directory: %s", err.message);
+    console.error("Error removing directory: %s", err.message);
   }
 };
 
-var createInstallDir = function () {
+var createDir = function (dirPath) {
   try {
-    fs.ensureDirSync(pluginInstallDir);
+    fs.ensureDirSync(dirPath);
   } catch (err) {
-    console.error("Error creating plugin installation directory: %s", err.message);
+    console.error("Error creating directory: %s", err.message);
   }
 };
 
-var copyPluginFiles = function () {
-  cleanInstallDir();
-  createInstallDir();
+var prepareFiles = function () {
+  var buildDir = localPath("build"),
+      copyList = ["gauge-proto", "src", "index.js", "js.json", "package.json", "README.md"];
+
+  cleanDir(buildDir);
+  createDir(buildDir);
+
+  copyList.forEach(function (item) {
+    try {
+      fs.copySync(localPath(item), path.join(buildDir, item), { clobber: true, filter: function (f) {
+        return !(/(\/.git|^\/build)/.test(f.split(localPath())[1]));
+      }});
+    } catch (err) {
+      console.error("Failed to copy %s to build directory: %s", item, err.message);
+      console.error(err.stack);
+    }
+  });
 
   try {
-    fs.copySync(localPath(), pluginInstallDir);
+    fs.removeSync(path.join(buildDir, "gauge-proto", ".git"));
+  } catch (err) {
+    console.error("Failed to remove .git in gauge-proto: %s", err.message);
+    console.error(err.stack);
+  }
+
+  try {
+    child_process.execSync("npm install --production", { cwd: buildDir });
+  } catch (err) {
+    console.error("Error installing modules from NPM: %s", err.message);
+    console.error(err.stack);
+  }
+
+};
+
+var installPluginFiles = function () {
+  cleanDir(pluginInstallDir);
+  createDir(pluginInstallDir);
+
+  prepareFiles();
+
+  try {
+    fs.copySync(localPath("build"), pluginInstallDir);
   } catch (err) {
     console.error("Failed to install plugin: %s", err.message);
+    console.error(err.stack);
   }
 
   console.log("Installed gauge-%s v%s", plugin.id, plugin.version);
 };
 
-copyPluginFiles();
+var createPackage = function () {
+  var zip = archiver("zip"),
+      deployDir = localPath("deploy"),
+      buildDir = localPath("build"),
+      packageFile = "gauge-" + plugin.id + "-" + plugin.version + ".zip";
+
+  cleanDir(deployDir);
+  createDir(deployDir);
+  prepareFiles();
+
+  var package = fs.createWriteStream(path.join(deployDir, packageFile));
+
+  zip.on("error", function (err) {
+    throw err;
+  });
+
+  package.on("close", function () {
+    console.log("Created: %s", path.join("deploy", packageFile));
+    console.log("To install this plugin, run:\n\t$ gauge --install js --file %s", path.join("deploy", packageFile));
+  });
+
+  zip.pipe(package);
+
+  zip.directory(buildDir, "/").finalize();
+
+};
+
+if (process.argv[2] === "--package") {
+  createPackage();
+} else {
+  installPluginFiles();
+}
