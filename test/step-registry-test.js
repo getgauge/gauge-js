8540cfe9d4ece5c5db5b2767409205ca52e9745e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-02-13 19:39:56 +0530

    Use beforeEach for step-registry tests
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- test/step-registry-test.js
+++ test/step-registry-test.js
@@ -2,11 +2,15 @@ var assert = require("chai").assert;
 var StepRegistry = require("../src/step-registry");
 
 describe("Store and retrieve steps", function() {
+  var sampleFunction,
+      stepRegistry;
+
+  beforeEach(function () {
+    sampleFunction = function() {};
+    stepRegistry = new StepRegistry();
+  });
 
   it("Should store and retrive steps", function(done) {
-    var sampleFunction = function() {};
-    var stepRegistry = new StepRegistry();
-
     stepRegistry.add("Sample Step {}", "Sample Step <1>", sampleFunction);
     assert.equal(sampleFunction, stepRegistry.get("Sample Step {}").fn);
     assert.equal("Sample Step {}", stepRegistry.get("Sample Step {}").generalisedText);
@@ -15,15 +19,12 @@ describe("Store and retrieve steps", function() {
   });
 
   it("Should return true for implemented step", function(done) {
-    var stepRegistry = new StepRegistry();
     stepRegistry.add("Say {} to {}", function(){});
     assert.equal(true, stepRegistry.exists("Say {} to {}"));
     done();
   });
 
   it("Should return false for unimplemented step", function(done) {
-    var stepRegistry = new StepRegistry();
-
     assert.equal(false, stepRegistry.exists("Say {} to {}"));
     done();
   });
