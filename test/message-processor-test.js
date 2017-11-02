1fd03b0980c59f12cb79a4c42d0d5f018e391ab6
BugDiver <vinaysh@thoughtworks.com>
2017-11-02 16:20:08 +0530

    Fixing tests.

--- test/message-processor-test.js
+++ test/message-processor-test.js
@@ -1,5 +1,4 @@
 var assert = require("chai").assert;
-var should = require("chai").should();
 var sinon = require("sinon");
 var protobuf = require("protobufjs");
 var stepRegistry = require("../src/step-registry");
@@ -47,7 +46,7 @@ describe("Step Validate Request Processing", function () {
 
   it("Should check if step exists in step registry when a StepValidateRequest is received", function (done) {
 
-    new MessageProcessor({message: message, errorType: {values: {}}}).getResponseFor(stepValidateRequest[0]);
+    new MessageProcessor({ message: message, errorType: { values: {} } }).getResponseFor(stepValidateRequest[0]);
 
     assert(stepRegistry.validate.calledOnce);
     assert.equal("A context step which gets executed before every scenario", stepRegistry.validate.getCall(0).args[0]);
@@ -56,7 +55,7 @@ describe("Step Validate Request Processing", function () {
   });
 
   it("StepValidateRequest should get back StepValidateResponse with isValid set to true if the step exists", function (done) {
-    var processor = new MessageProcessor({message: message});
+    var processor = new MessageProcessor({ message: message });
     processor.on("messageProcessed", function (response) {
       assert.deepEqual(stepValidateRequest[1].messageId, response.messageId);
       assert.equal(message.MessageType.StepValidateResponse, response.messageType);
@@ -67,7 +66,7 @@ describe("Step Validate Request Processing", function () {
   });
 
   it("StepValidateRequest should get back StepValidateResponse with isValid set to false if the step does not exist", function (done) {
-    var processor = new MessageProcessor({message: message, errorType: {values: {}}});
+    var processor = new MessageProcessor({ message: message, errorType: { values: {} } });
     processor.on("messageProcessed", function (response) {
       assert.deepEqual(stepValidateRequest[0].messageId, response.messageId);
       assert.equal(message.MessageType.StepValidateResponse, response.messageType);
@@ -80,7 +79,7 @@ describe("Step Validate Request Processing", function () {
 });
 
 describe("StepNameRequest Processing", function () {
-  var stepNameRequest= [];
+  var stepNameRequest = [];
   var message = null;
   var sandbox;
   this.timeout(10000);
@@ -105,12 +104,12 @@ describe("StepNameRequest Processing", function () {
     });
   });
 
-  after( function () {
+  after(function () {
     sandbox.restore();
   });
 
   it("StepNameRequest should get back StepNameResponse with fileName and lineNumber", function (done) {
-    var processor = new MessageProcessor({message: message, errorType: {values: {}}});
+    var processor = new MessageProcessor({ message: message, errorType: { values: {} } });
     processor.on("messageProcessed", function (response) {
       assert.deepEqual(stepNameRequest.messageId, response.messageId);
       assert.equal(message.MessageType.StepNameResponse, response.messageType);
@@ -123,20 +122,20 @@ describe("StepNameRequest Processing", function () {
 });
 
 describe("StepPositionsRequest Processing", function () {
-  var stepPositionsRequest= [];
+  var stepPositionsRequest = [];
   var message = null;
   this.timeout(10000);
   before(function (done) {
     var filePath = "example.js";
     cacheRegistry.add(filePath, "\"use strict\";\n" +
-"var assert = require(\"assert\");\n"+
-"var vowels = require(\"./vowels\");\n" +
-"step(\"Vowels in English language are <vowels>.\", function(vowelsGiven) {\n"+
-"  assert.equal(vowelsGiven, vowels.vowelList.join(\"\"));\n"+
-"});\n"+
-"step(\"The word <word> has <number> vowels.\", function(word, number) {\n"+
-"  assert.equal(number, vowels.numVowels(word));\n"+
-"});");
+      "var assert = require(\"assert\");\n" +
+      "var vowels = require(\"./vowels\");\n" +
+      "step(\"Vowels in English language are <vowels>.\", function(vowelsGiven) {\n" +
+      "  assert.equal(vowelsGiven, vowels.vowelList.join(\"\"));\n" +
+      "});\n" +
+      "step(\"The word <word> has <number> vowels.\", function(word, number) {\n" +
+      "  assert.equal(number, vowels.numVowels(word));\n" +
+      "});");
     protobuf.load("gauge-proto/messages.proto").then(function (root) {
       message = root.lookupType("gauge.messages.Message");
       stepPositionsRequest =
@@ -152,11 +151,11 @@ describe("StepPositionsRequest Processing", function () {
   });
 
   it("StepPositionsRequest should get back StepPositionsResponse with stepValue and lineNumber", function (done) {
-    var processor = new MessageProcessor({message: message, errorType: {values: {}}});
+    var processor = new MessageProcessor({ message: message, errorType: { values: {} } });
     processor.on("messageProcessed", function (response) {
       assert.deepEqual(stepPositionsRequest.messageId, response.messageId);
       assert.equal(message.MessageType.StepPositionsResponse, response.messageType);
-      should.not.exist(response.stepPositionsResponse.error);
+      assert.equal("", response.stepPositionsResponse.error);
       assert.equal(2, response.stepPositionsResponse.stepPositions.length);
       assert.equal(1, response.stepPositionsResponse.stepPositions.filter(function (stepPosition) {
         return stepPosition.stepValue === "Vowels in English language are <vowels>." && stepPosition.lineNumber === 4;
