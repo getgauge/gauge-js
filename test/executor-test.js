53bfc92b2999590d0b6366b3f504f88b539069d3
BugDiver <vinaysh@thoughtworks.com>
2017-11-22 17:05:12 +0530

    choping stacktrace #71

--- test/executor-test.js
+++ test/executor-test.js
@@ -1,23 +1,23 @@
 var expect = require("chai").expect;
-var sinon  = require("sinon");
+var sinon = require("sinon");
 var protobuf = require("protobufjs");
 var executor = require("../src/executor");
 var stepRegistry = require("../src/step-registry");
 
 
-describe("Executing steps", function() {
+describe("Executing steps", function () {
   var executeStepMessage = null;
   var executeStepMessageFailing = null;
   var message = null;
   this.timeout(10000);
 
-  before( function(done) {
+  before(function (done) {
     var opts = { continueOnFailure: false };
     stepRegistry.clear();
-    stepRegistry.add("Say {} to {}", "Say <hi> to <me>", function() {}, "executor-test.js",3, opts);
-    stepRegistry.add("failing test", "failing test", function() {throw "Error";}, "executor-test.js",6, opts);
+    stepRegistry.add("Say {} to {}", "Say <hi> to <me>", function () { }, "executor-test.js", 3, opts);
+    stepRegistry.add("failing test", "failing test", function () { throw new Error("error message"); }, "executor-test.js", 6, opts);
     sinon.spy(stepRegistry, "get");
-    protobuf.load("gauge-proto/messages.proto").then(function(root){
+    protobuf.load("gauge-proto/messages.proto").then(function (root) {
       message = root.lookupType("gauge.messages.Message");
       executeStepMessage = message.create({
         messageId: 1,
@@ -44,27 +44,29 @@ describe("Executing steps", function() {
     });
   });
 
-  after( function(done) {
+  after(function (done) {
     stepRegistry.get.restore();
     done();
   });
 
-  it("Should resolve promise when test function passes", function(done) {
+  it("Should resolve promise when test function passes", function (done) {
     var promise = executor.step(executeStepMessage, message);
     promise.then(
-      function(value) {
+      function (value) {
         expect(value.executionStatusResponse.executionResult.failed).to.equal(false);
         done();
       }
     ).done();
   });
 
-  it("Should reject the promise when test function fails", function(done) {
+  it("Should reject the promise when test function fails", function (done) {
     var promise = executor.step(executeStepMessageFailing, message);
     promise.then(
-      function() {},
-      function(reason) {
+      function () { },
+      function (reason) {
         expect(reason.executionStatusResponse.executionResult.failed).to.equal(true);
+        expect(reason.executionStatusResponse.executionResult.errorMessage).to.eql("Error: error message");
+        expect(reason.executionStatusResponse.executionResult.stackTrace).to.contains("executor-test.js");
         done();
       }
     ).done();
