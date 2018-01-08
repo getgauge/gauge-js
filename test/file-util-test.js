7d9b73396b7389d8e1374f5cea48a31e732d26a5
Apoorva G A <apoorvag@thoughtworks.com>
2018-01-08 13:57:52 +0530

    Running tests checking for windows path on windows only

--- test/file-util-test.js
+++ test/file-util-test.js
@@ -1,33 +1,50 @@
 var assert = require("chai").assert;
 var fileUtil = require("../src/file-util");
+var isWindows = require("check-if-windows");
 
 describe("File util functions", function () {
-  
+
   it("Should return true when given paths are same windows", function () {
-    var path1 = "c:/Users/user_name/test_js/tests/step_implementation.js";
-    var path2 = "C:/Users/user_name/test_js/tests/step_implementation.js";
-    
-    assert.isTrue(fileUtil.isSameFilePath(path1, path2));
+    if (!isWindows){
+      this.skip();
+    } else {
+      var path1 = "c:/Users/user_name/test_js/tests/step_implementation.js";
+      var path2 = "C:/Users/user_name/test_js/tests/step_implementation.js";
+
+      assert.isTrue(fileUtil.isSameFilePath(path1, path2));
+    }
   });
 
   it("Should return true when given paths are with different fileseperator ", function () {
-    var path1 = "c:\\Users\\user_name\\test_js\\tests\\step_implementation.js";
-    var path2 = "c:/Users/user_name/test_js/tests/step_implementation.js";
+    if (!isWindows){
+      this.skip();
+    } else  {
+      var path1 = "c:\\Users\\user_name\\test_js\\tests\\step_implementation.js";
+      var path2 = "c:/Users/user_name/test_js/tests/step_implementation.js";
+      
+      assert.isTrue(fileUtil.isSameFilePath(path1, path2));
+    }
+  });
+
+  it("Should return true when given paths are same and has space ", function () {
+    var path1 = "/Users/test_js/tests/Step implementation.js";
+    var path2 = "/Users/test_js/tests/Step implementation.js";
     
     assert.isTrue(fileUtil.isSameFilePath(path1, path2));
   });
 
+
   it("Should return true when given paths are same on linux ", function () {
     var path1 = "/Users/test_js/tests/Step_implementation.js";
-    var path2 = "/Users/test_js/tests/step_implementation.js";
-    
+    var path2 = "/Users/test_js/tests/Step_implementation.js";
+
     assert.isTrue(fileUtil.isSameFilePath(path1, path2));
   });
 
   it("Should return false when given paths are not different", function () {
     var path1 = "/Users/test_js/tests/Step_implementation.js";
     var path2 = "/Users/test_js/tests1/step_implementation.js";
-    
+
     assert.isFalse(fileUtil.isSameFilePath(path1, path2));
   });
 
