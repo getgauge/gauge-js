67b58e177a292e3cfffc17a1c3874fa269526f4f
BugDiver <vinaysh@thoughtworks.com>
2018-03-02 14:27:32 +0530

    Ignoring node_modules, reports while scanning files. #114

--- src/file-util.js
+++ src/file-util.js
@@ -1,33 +1,27 @@
 var fs = require("fs");
 var path = require("path");
+var klawSync = require("klaw-sync");
 var micromatch = require("micromatch");
+
+var ignoredDirs = ['.gauge', '.git', 'node_modules', 'reports'];
+
+
+function shouldIgnore(item) {
+  return ignoredDirs.includes(path.basename(item.path))
+}
 
 exports = module.exports;
 
-exports.getListOfFilesFromPath = function(basePath, conf) {
-  if(!fs.existsSync(basePath)) {
-    return [];
-  }
-  function walk(dir){
-    var result = [];
-  
-    fs.readdirSync(dir).forEach(function(fileName){
-      var filePath = path.join(dir, fileName);
-      var stat = fs.statSync(filePath);
-      if (stat && !stat.isDirectory()) {
-        result.push(filePath);
-      }else{
-        var dirContent = walk(filePath);
-        result = result.concat(dirContent);
-      }
-    });
-    return result;
-  }
-  var result = walk(basePath);
-  result = micromatch(result, conf.testMatch);
-  return result;
+exports.getListOfFilesFromPath = function (basePath, conf) {
+  if (!fs.existsSync(basePath)) return [];
+  var options = { filter: shouldIgnore, noRecurseOnFailedFilter: true }
+  var results = klawSync(basePath, options).map(function (item) {
+    return item.path;
+  });
+  results = micromatch(results, conf.testMatch);
+  return results;
 };
 
-exports.isSameFilePath = function(filePath1, filePath2) {
+exports.isSameFilePath = function (filePath1, filePath2) {
   return path.relative(filePath1, filePath2) === "";
 };
