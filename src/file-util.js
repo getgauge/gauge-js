06c7d6ec70c4e9fa914fbfef0df10b766674434a
BugDiver <vinaysh@thoughtworks.com>
2018-03-02 14:31:07 +0530

    Fixed check styles

--- src/file-util.js
+++ src/file-util.js
@@ -3,18 +3,20 @@ var path = require("path");
 var klawSync = require("klaw-sync");
 var micromatch = require("micromatch");
 
-var ignoredDirs = ['.gauge', '.git', 'node_modules', 'reports'];
+var ignoredDirs = [".gauge", ".git", "node_modules", "reports"];
 
 
 function shouldIgnore(item) {
-  return ignoredDirs.includes(path.basename(item.path))
+  return ignoredDirs.includes(path.basename(item.path));
 }
 
 exports = module.exports;
 
 exports.getListOfFilesFromPath = function (basePath, conf) {
-  if (!fs.existsSync(basePath)) return [];
-  var options = { filter: shouldIgnore, noRecurseOnFailedFilter: true }
+  if (!fs.existsSync(basePath)) {
+    return [];
+  }
+  var options = { filter: shouldIgnore, noRecurseOnFailedFilter: true };
   var results = klawSync(basePath, options).map(function (item) {
     return item.path;
   });
