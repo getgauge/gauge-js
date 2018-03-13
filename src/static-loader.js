74a74d2c31381d88d5a72ddc0a367d4017a97829
nehashri <nehashrl@thoughtworks.com>
2018-03-13 18:15:53 +0530

    temp

--- src/static-loader.js
+++ src/static-loader.js
@@ -60,8 +60,8 @@ function createAst(content) {
   }
 }
 
-function loadFiles(projectRoot) {
-  fileUtil.getListOfFiles(projectRoot).forEach(function (filePath) {
+function loadFiles() {
+  fileUtil.getListOfFiles().forEach(function (filePath) {
     var ast = createAst(fs.readFileSync(filePath, "UTF-8"));
     if (ast) {
       loadFile(filePath, ast);
