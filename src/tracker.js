0cc73c736f47249ebfc22be5f18d7f96c37725fd
Subham Singh <s.chauhan5505@gmail.com>
2018-12-04 11:43:00 +0530

    Fixed to run js project without package.json, #176

--- src/tracker.js
+++ src/tracker.js
@@ -11,18 +11,33 @@ var isCI = function () {
 };
 
 var getProjectInfo = function () {
-  var data = fs.readFileSync(path.join(process.env.GAUGE_PROJECT_ROOT, "package.json"), "utf-8");
-  return JSON.parse(data);
+  fs.open(path.join(process.env.GAUGE_PROJECT_ROOT, "package.json"), 'r', function (err, fd) {
+    if (err && err.code === 'ENOENT') {
+      return;
+    }
+    var data = fs.readFileSync(path.join(process.env.GAUGE_PROJECT_ROOT, "package.json"), "utf-8");
+    return JSON.parse(data);
+  });
 };
 
 var getPostData = function (medium, cid) {
   var pi = getProjectInfo();
+  if (!pi) {
+    return "";
+  }
   var labels = pi.dependencies.taiko && "taiko," + os.platform() || os.platform();
   return qs.stringify({
-    aip: "1", an: "Gauge Core", av: version, cid: cid,
-    cm: medium, cs: "Gauge Core",
-    ea: "js", ec: "runner", el: labels,
-    t: "event", v: "1",
+    aip: "1",
+    an: "Gauge Core",
+    av: version,
+    cid: cid,
+    cm: medium,
+    cs: "Gauge Core",
+    ea: "js",
+    ec: "runner",
+    el: labels,
+    t: "event",
+    v: "1",
     tid: "UA-54838477-1"
   });
 };
@@ -43,21 +58,28 @@ var getPostOptions = function (data) {
 
 
 var send = function (medium) {
-  if (!JSON.parse(process.env.GAUGE_TELEMETRY_ENABLED)) { return Promise.resolve(); }
-  if (medium === "console" && isCI()) { medium = "CI"; }
+  if (!JSON.parse(process.env.GAUGE_TELEMETRY_ENABLED)) {
+    return;
+  }
+  if (medium === "console" && isCI()) {
+    medium = "CI";
+  }
   var data = getPostData(medium, process.env.GAUGE_UNIQUE_INSTALLATION_ID);
-  return new Promise(function (resolve, reject) {
-    var req = http.request(getPostOptions(data), function (res) {
-      res.setEncoding("utf8");
-      res.on("end", resolve);
-      res.on("error", reject);
-    });
-    req.write(data);
-    req.end();
+  var req = http.request(getPostOptions(data), function (res) {
+    res.setEncoding("utf8");
   });
+  req.on('error', (e) => {
+    console.error(`Connection Error: ${e.message}`);
+  });
+  req.write(data);
+  req.end();
 };
 
 module.exports = {
-  trackConsole: function () { return send("console"); },
-  trackLSP: function () { return send("deamon"); }
+  trackConsole: function () {
+    send("console");
+  },
+  trackLSP: function () {
+    send("deamon");
+  }
 };
