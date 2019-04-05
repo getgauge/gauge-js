b3ff9b05eb67c125a2426aa22aeb4e5c363c1edb
NivedhaSenthil <nivedhasenthil@gmail.com>
2019-04-05 17:42:57 +0530

    #191 Load implementation only once

--- src/impl-loader.js
+++ src/impl-loader.js
@@ -1,13 +1,23 @@
 var fileUtil = require("./file-util"),
   VM = require("./vm");
+var loaded = false;
+var loading = false;  
 
-function loadImpl() {
+function loadImpl(stepRegistry) {
+  if(loaded) {return;}
+  while(loading){
+    if(loaded) {return;}
+  }
+  loading = true;
+  stepRegistry.clear();
   var vm = new VM();
   fileUtil.getListOfFiles().forEach(function(filePath) {
     process.env.GAUGE_STEPFILEPATH = filePath;
     vm.contextify(filePath, process.env.GAUGE_PROJECT_ROOT);
     vm.runFile(filePath);
   });
+  loading = false;
+  loaded = true;
 }
 
 module.exports= {
