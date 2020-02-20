815ebf88a03bb5f91bbf9fea6e37742627a879c4
Dharmendra Singh <dharmenn@thoughtworks.com>
2020-02-20 12:39:58 +0530

    Write base64 screenshot data with proper encoding. (#296)
    * Write base64 screenshot data with proper encoding. #295
    
    * Fixed review comments
    
    Co-authored-by: Vinay Shankar Shukla <vinaysh@thoughtworks.com>

--- test/screenshot-test.js
+++ test/screenshot-test.js
@@ -15,9 +15,9 @@ function asyncScreenshotFunction() {
 }
 
 describe("screentshot.capture", function () {
-  const screenshotsDir =  path.join(".gauge", "screenshots");
+  const screenshotsDir = path.join(".gauge", "screenshots");
 
-  this.beforeEach( () => {
+  this.beforeEach(() => {
     process.env.gauge_screenshots_dir = screenshotsDir;
   });
   afterEach(() => {
@@ -44,64 +44,104 @@ describe("screentshot.capture", function () {
     afterEach(function () {
       global.gauge = { screenshotFn: null };
     });
-    it("Should capture screentshot with async function", function (done) {
-      sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
-      const screenShotFile = "screenshot-6767787989089.png";
-      global.gauge = { screenshotFn: asyncScreenshotFunction };
-      const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
+    describe("when data is in base64 string", () => {
+      it("Should capture screentshot with async function", function (done) {
+        sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
+        const screenShotFile = "screenshot-6767787989089.png";
+        global.gauge = { screenshotFn: asyncScreenshotFunction };
+        const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
 
-      screenshot.capture().then(function (file) {
-        const filePath = path.join(screenshotsDir, screenShotFile);
-        const expectedArgs = [filePath, Buffer.from("screentshot").toString("base64")];
-        const actualArgs = writeFileSyncStub.getCall(0).args;
-        expect(file).to.be.equal(screenShotFile);
-        expect(actualArgs).to.be.deep.equal(expectedArgs);
-        done();
+        screenshot.capture().then(function (file) {
+          const filePath = path.join(screenshotsDir, screenShotFile);
+          const expectedArgs = [filePath, Buffer.from("screentshot").toString("base64"), "base64"];
+          const actualArgs = writeFileSyncStub.getCall(0).args;
+          expect(file).to.be.equal(screenShotFile);
+          expect(actualArgs).to.be.deep.equal(expectedArgs);
+          done();
+        });
+      });
+
+      it("Should capture screentshot with sync function", function (done) {
+        sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
+        const screenShotFile = "screenshot-6767787989089.png";
+        global.gauge = { screenshotFn: screenshotFunction };
+        const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
+
+        screenshot.capture().then(function (file) {
+          const filePath = path.join(screenshotsDir, screenShotFile);
+          const expectedArgs = [filePath, Buffer.from("screentshot").toString("base64"), "base64"];
+          const actualArgs = writeFileSyncStub.getCall(0).args;
+          expect(file).to.be.equal(screenShotFile);
+          expect(actualArgs).to.be.deep.equal(expectedArgs);
+          done();
+        });
+      });
+
+      describe("when data is in bytes", () => {
+        it("Should capture screentshot with function returning a promise", function (done) {
+          sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
+          const screenShotFile = "screenshot-6767787989089.png";
+          global.gauge = { screenshotFn: () => Promise.resolve(Buffer.from("screentshot")) };
+          const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
+
+          screenshot.capture().then(function (file) {
+            const filePath = path.join(screenshotsDir, screenShotFile);
+            const expectedArgs = [filePath, Buffer.from("screentshot")];
+            const actualArgs = writeFileSyncStub.getCall(0).args;
+            expect(file).to.be.equal(screenShotFile);
+            expect(actualArgs).to.be.deep.equal(expectedArgs);
+            done();
+          });
+        });
+
+        it("Should capture screentshot with function returning screenshot data", function (done) {
+          sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
+          const screenShotFile = "screenshot-6767787989089.png";
+          global.gauge = { screenshotFn: ()=> Buffer.from("screentshot") };
+          const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
+
+          screenshot.capture().then(function (file) {
+            const filePath = path.join(screenshotsDir, screenShotFile);
+            const expectedArgs = [filePath, Buffer.from("screentshot")];
+            const actualArgs = writeFileSyncStub.getCall(0).args;
+            expect(file).to.be.equal(screenShotFile);
+            expect(actualArgs).to.be.deep.equal(expectedArgs);
+            done();
+          });
+        });
       });
     });
 
-    it("Should capture screentshot with sync function", function (done) {
-      sandbox.stub(process.hrtime, "bigint").returns(6767787989089);
-      const screenShotFile = "screenshot-6767787989089.png";
-      global.gauge = { screenshotFn: screenshotFunction };
-      const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
-
-      screenshot.capture().then(function (file) {
-        const filePath = path.join(screenshotsDir, screenShotFile);
-        const expectedArgs = [filePath, Buffer.from("screentshot").toString("base64")];
-        const actualArgs = writeFileSyncStub.getCall(0).args;
-        expect(file).to.be.equal(screenShotFile);
-        expect(actualArgs).to.be.deep.equal(expectedArgs);
-        done();
+    describe("with custom screenshot writer function", () => {
+      afterEach(() => {
+        global.gauge = { customScreenshotWriter: null };
       });
-    });
-  });
+      it("Should capture screentshot with function returning a promise", function (done) {
+        const screenShotFile = "screenshot-file-1.png";
+        global.gauge = {
+          customScreenshotWriter: function () {
+            return Promise.resolve(screenShotFile);
+          }
+        };
 
-  describe("with custom screenshot writer function", () => {
-    afterEach(() => {
-      global.gauge = { customScreenshotWriter: null };
-    });
-    it("Should capture screentshot with async function", function (done) {
-      const screenShotFile = "screenshot-file-1.png";
-      global.gauge = { customScreenshotWriter: function() {
-        return Promise.resolve(screenShotFile);
-      } };
-
-      screenshot.capture().then(function (file) {
-        expect(file).to.be.equal(screenShotFile);
-        done();
+        screenshot.capture().then(function (file) {
+          expect(file).to.be.equal(screenShotFile);
+          done();
+        });
       });
-    });
 
-    it("Should capture screentshot with sync function", function (done) {
-      const screenShotFile = "screenshot-file-2.png";
-      global.gauge = { customScreenshotWriter: function() {
-        return screenShotFile;
-      } };
+      it("Should capture screentshot with function returning screenshot file name", function (done) {
+        const screenShotFile = "screenshot-file-2.png";
+        global.gauge = {
+          customScreenshotWriter: function () {
+            return screenShotFile;
+          }
+        };
 
-      screenshot.capture().then(function (file) {
-        expect(file).to.be.equal(screenShotFile);
-        done();
+        screenshot.capture().then(function (file) {
+          expect(file).to.be.equal(screenShotFile);
+          done();
+        });
       });
     });
   });
