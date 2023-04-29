364a73e996c41b2722a26969bcc81fff65d5dd47
autumn-calhoun <58706027+autumn-calhoun@users.noreply.github.com>
2023-04-29 14:48:21 -0400

    Added another function to Table that will process async callback func… (#571)
    * Added another function to Table that will process async callback functions to resolve issue #570.  Also had to bump the ecmaVersion to 8 in eslint to support async await
    
    Signed-off-by: Autumn Calhoun <autumn.calhoun@crowncastle.com>
    
    * #570 updated per code review comments
    
    Signed-off-by: Autumn Calhoun <autumn.calhoun@crowncastle.com>
    
    ---------
    
    Signed-off-by: Autumn Calhoun <autumn.calhoun@crowncastle.com>

--- src/table.js
+++ src/table.js
@@ -1,11 +1,17 @@
 var Table = function (protoTable) {
   Object.assign(this, protoTable);
 
-  this.entries = function (callback) {
-    for (var row of this.rows) {
+  this.entries = async function (callback) {
+    const AsyncFunction = (async () => {}).constructor;
+
+    for (const row of this.rows) {
       let entry = {};
       row.cells.forEach((cell, index) => entry[this.headers.cells[index]] = cell);
-      callback(entry);
+      if(callback instanceof AsyncFunction) {
+        await callback(entry);
+      } else {
+        callback(entry);
+      }
     }
   };
 };
--- test/table.js
+++ test/table.js
@@ -1,5 +1,7 @@
 var Table = require("../src/table");
 var expect = require("chai").expect;
+const util = require("util");
+const setTimeoutPromise = util.promisify(setTimeout);
 
 describe("ProtoTable parsing", function() {
 
@@ -16,6 +18,14 @@ describe("ProtoTable parsing", function() {
   };
 
   var table = new Table(protoTable);
+
+  let getRowData = function(entry) {
+    const rowData = setTimeoutPromise(500, entry).then((value) => {
+      return {cells: [value["Product"], value["Description"]]};
+    });
+    return rowData;
+
+  };
 
   it("Should get headers", function () {
     expect(table.headers).to.deep.equal(protoTable.headers);
@@ -41,6 +51,15 @@ describe("ProtoTable parsing", function() {
         "Description": "Agile project management"
       });
     });
+
+    it("Should process an asynchronous callback action", async function () {
+      let data = [];
+
+      await table.entries(async (entry) => data.push(await getRowData(entry)));
+
+      expect(data).to.deep.equal(protoTable.rows);
+    }).timeout(10000);
+
   });
 
 });
