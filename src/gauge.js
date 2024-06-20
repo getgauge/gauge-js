437cab75c359fd69a5dfc9b01b066c47424cf6a0
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-06-20 20:07:31 +0100

    shim __dirname
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- src/gauge.js
+++ src/gauge.js
@@ -1,9 +1,12 @@
-import gaugeGlobal from "./gauge-global.js";
-import protobuf from "protobufjs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import protoLoader from "@grpc/proto-loader";
-import path from "path";
+import protobuf from "protobufjs";
+import gaugeGlobal from "./gauge-global.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import loader from "./static-loader.js";
-const PROTO_PATH = __dirname + "/../gauge-proto/services.proto";
+const PROTO_PATH = `${__dirname}/../gauge-proto/services.proto`;
 import grpc from "@grpc/grpc-js";
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
@@ -11,39 +14,56 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   longs: String,
   enums: String,
   defaults: true,
-  oneofs: true
+  oneofs: true,
 });
 
-const servicesProto = grpc.loadPackageDefinition(packageDefinition).gauge.messages;
+const servicesProto =
+  grpc.loadPackageDefinition(packageDefinition).gauge.messages;
 
-import ServiceHandlers from "./serviceHandlers.js";
 import logger from "./logger.js";
+import ServiceHandlers from "./serviceHandlers.js";
 
 function run() {
   global.gauge = gaugeGlobal.gauge;
-  protobuf.load(path.resolve("gauge-proto/messages.proto")).then(function (root) {
-    var errorType = root.lookupEnum("gauge.messages.StepValidateResponse.ErrorType");
-    var fileStatus = root.lookupEnum("gauge.messages.CacheFileRequest.FileStatus");
-    return { errorType: errorType, fileStatus: fileStatus };
-  }).catch(function (e) {
-    logger.error("Failed while loading runner.\n" + e);
-    process.exit();
-  }).then(function (types) {
-    loader.load();
-    var server = new grpc.Server();
-    server.addService(servicesProto.Runner.service, new ServiceHandlers(server, types));
-    server.bindAsync("127.0.0.1:0", grpc.ServerCredentials.createInsecure(), (err, port) => {
-      if (!err) {
-        logger.info("Listening on port:" + port);
-        server.start();
-      } else {
-        logger.error(err);
-        process.exit();
-      }
+  protobuf
+    .load(path.resolve("gauge-proto/messages.proto"))
+    .then((root) => {
+      const errorType = root.lookupEnum(
+        "gauge.messages.StepValidateResponse.ErrorType",
+      );
+      const fileStatus = root.lookupEnum(
+        "gauge.messages.CacheFileRequest.FileStatus",
+      );
+      return { errorType: errorType, fileStatus: fileStatus };
+    })
+    .catch((e) => {
+      logger.error(`Failed while loading runner.\n${e}`);
+      process.exit();
+    })
+    .then((types) => {
+      loader.load();
+      const server = new grpc.Server();
+      server.addService(
+        servicesProto.Runner.service,
+        new ServiceHandlers(server, types),
+      );
+      server.bindAsync(
+        "127.0.0.1:0",
+        grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+          if (!err) {
+            logger.info(`Listening on port:${port}`);
+            server.start();
+          } else {
+            logger.error(err);
+            process.exit();
+          }
+        },
+      );
+    })
+    .catch((e) => {
+      logger.error(`${e.message}\n${e.stack}`);
     });
-  }).catch(function (e) {
-    logger.error(`${e.message}\n${e.stack}`);
-  });
 }
 
 if (process.argv[2] === "--run") {
@@ -51,5 +71,5 @@ if (process.argv[2] === "--run") {
 }
 
 export default {
-  run: run
+  run: run,
 };
