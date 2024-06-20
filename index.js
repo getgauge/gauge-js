f797f5c910571dc08ebaa57526aba01a3e64b5a3
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-06-20 19:57:25 +0100

    Fix typo
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-#! /usr/bin/env nod
+#! /usr/bin/env node
 
 const version = process.versions.node.split(".");
-if (parseInt(version[0]) < 16) {
-  throw new Error("gauge-js requires Node.js version 16+. Current version: " + process.versions.node);
+if (Number.parseInt(version[0]) < 16) {
+  throw new Error(
+    "gauge-js requires Node.js version 16+. Current version: " +
+    process.versions.node,
+  );
 }
 
 import fs from "fs";
@@ -16,48 +19,53 @@ const skeldir = path.join(__dirname, "skel"),
   jsPropertyFile = "js.properties";
 
 if (process.argv[2] === "--init") {
-
   console.log("Initialising Gauge JavaScript project");
-  fs.mkdir(srcdir, 484, function (err) {
+  fs.mkdir(srcdir, 484, (err) => {
     if (err && err.code !== "EEXIST") {
       console.error(err);
     } else {
-      fs.createReadStream(path.join(skeldir, testCode))
-        .pipe(fs.createWriteStream(path.join(srcdir, testCode)));
+      fs.createReadStream(path.join(skeldir, testCode)).pipe(
+        fs.createWriteStream(path.join(srcdir, testCode)),
+      );
     }
   });
 
-  fs.mkdir(path.dirname(envdir), 484, function (err) {
+  fs.mkdir(path.dirname(envdir), 484, (err) => {
     if (err && err.code !== "EEXIST") {
       console.error(err);
     } else {
-      fs.mkdir(envdir, 484, function (err) {
+      fs.mkdir(envdir, 484, (err) => {
         if (err && err.code !== "EEXIST") {
           console.error(err);
         } else {
-          fs.createReadStream(path.join(skeldir, jsPropertyFile))
-            .pipe(fs.createWriteStream(path.join(envdir, jsPropertyFile)));
+          fs.createReadStream(path.join(skeldir, jsPropertyFile)).pipe(
+            fs.createWriteStream(path.join(envdir, jsPropertyFile)),
+          );
         }
       });
     }
   });
-}
-
-else if (process.argv[2] === "--start") {
+} else if (process.argv[2] === "--start") {
   let args = ["./src/gauge.js", "--run"];
   if (process.env.gauge_nodejs_args) {
     args = process.env.gauge_nodejs_args.split(" ").concat(args);
   }
   const cmd = process.env.gauge_nodejs_bin || "node";
-  const runner = child_process.spawn(cmd, args, {env: process.env, silent: false, stdio: "inherit"});
+  const runner = child_process.spawn(cmd, args, {
+    env: process.env,
+    silent: false,
+    stdio: "inherit",
+  });
   process.on("beforeExit", (code) => {
     try {
-      if (!runner.killed) { runner.kill("SIGINT"); }
+      if (!runner.killed) {
+        runner.kill("SIGINT");
+      }
     } finally {
       process.exit(code);
     }
   });
-  runner.on("error", function (err) {
+  runner.on("error", (err) => {
     console.trace(err.stack);
   });
 }
